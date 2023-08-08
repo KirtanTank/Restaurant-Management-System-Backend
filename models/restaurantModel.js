10e3353f065028f597544f815e88910e4d985666
Kirtan Tank <tankkirtan5@gmail.com>
2023-08-09 02:04:40 +0530

    changed datatype of contact,get rid of int limit

--- models/restaurantModel.js
+++ models/restaurantModel.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
         contact: {
-            type: DataTypes.INTEGER,
+            type: DataTypes.STRING,
             allowNull: false
         },
         added_by: {
